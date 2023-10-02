fbecf05ab0cb592c0e4c59e6c1fb1775b0addc9d
Rahul Yadav <133594357+RahulYadav063@users.noreply.github.com>
2023-10-02 13:19:06 +0530

    Add files via upload

---
+++ details.py
@@ -0,0 +1,7 @@
+api_id = 20543083
+api_hash = "8c0491f39646893e947098b6133c44cb"
+bot_token = "6420933334:AAHxuMNEVI5N9LaO1bF-3ArFC2sX59qxIlw"
+auth_users = [6123937721, -1001855559473]
+sudo_user = 6123937721
+log_channel = -1001768085420
+txt_channel = -1001863602838
