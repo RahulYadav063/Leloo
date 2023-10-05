81ed7feaa8359baa9ad4584bb8e86536637fe4f1
Rahul Yadav <133594357+RahulYadav063@users.noreply.github.com>
2023-10-05 06:25:45 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,7 +1,7 @@
 api_id = 20543083
 api_hash = "8c0491f39646893e947098b6133c44cb"
-bot_token = "6420933334:AAHxuMNEVI5N9LaO1bF-3ArFC2sX59qxIlw"
-auth_users = [6123937721, -1001855559473]
+bot_token = "6454742390:AAEo86rRMuxd5VV3Yr4vNwKPHYLVbggbLVM"
+auth_users = [6123937721, 5685862369]
 sudo_user = 6123937721
-log_channel = -1001768085420
+log_channel = -1001903653148
 txt_channel = -1001863602838
