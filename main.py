3d03176fd86484d269ddd8f2091c88db8933509f
Rahul Yadav <133594357+RahulYadav063@users.noreply.github.com>
2023-10-05 06:51:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -225,7 +225,7 @@ async def account_login(bot: Client, m: Message):
                     res_file = await helper.download_video(url, cmd, name)
                     filename = res_file
                     await prog.delete(True)
-                    await helper.send_vid(bot, m, cc, filename, thumb, name)
+                    await helper.send_vid(bot, m, cc, filename, thumb, name, prog)
                     count += 1
 
             except Exception as e:
